e250aeb1cdf1fd444648330de13b30c1b5d7a8fd
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-09-09 22:46:58 +0530

    link to register page added to login page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -54,7 +54,7 @@ export default function Navbar() {
                     }
                     to='/login'
                 >
-                    Login/Signup
+                    Login/Register
                 </NavLink>
             )}
         </nav>
