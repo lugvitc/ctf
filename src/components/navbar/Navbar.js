99378f6acd63a5d77a4459ca39b4bc80448486c4
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-09-09 22:33:07 +0530

    added new and improved navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,26 +1,58 @@
 import { NavLink } from 'react-router-dom';
+import useTeam from '../../hooks/useTeam';
 import styles from './Navbar.module.css';
 
+const links = [
+    { to: '/', text: 'Home' },
+    // { to: '/register', text: 'Register' },
+    // { to: '/login', text: 'Login' },
+    { to: '/play', text: 'Play' },
+    { to: '/leaderboard', text: 'Leaderboard' }
+];
+
 export default function Navbar() {
+    const { team, logoutTeam } = useTeam();
+
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to='/'>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/register'>Register</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/login'>Login</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/play'>Play</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/leaderboard'>Leaderboard</NavLink>
-                </li>
-            </ul>
+        <nav className={styles.all}>
+            <div className={styles.logos}>LUG x TRC</div>
+            <div className={styles.links}>
+                {links.map(link => (
+                    <NavLink
+                        className={({ isActive }) =>
+                            `${styles.link} ${isActive ? styles.active : null}`
+                        }
+                        key={link.to}
+                        to={link.to}
+                    >
+                        {link.text}
+                    </NavLink>
+                ))}
+            </div>
+            {team ? (
+                <>
+                    <NavLink
+                        className={({ isActive }) =>
+                            `${styles.link} ${isActive ? styles.active : null}`
+                        }
+                        to='/team'
+                    >
+                        Team {team.name}: {team.currentPoints || 0} points
+                    </NavLink>
+                    <div className={styles.link} onClick={logoutTeam}>
+                        Logout
+                    </div>
+                </>
+            ) : (
+                <NavLink
+                    className={({ isActive }) =>
+                        `${styles.link} ${isActive ? styles.active : null}`
+                    }
+                    to='/login'
+                >
+                    Login/Signup
+                </NavLink>
+            )}
         </nav>
     );
 }
