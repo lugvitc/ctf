8a810610d279ac36ffa9b8421ecdc9a9e77bd15d
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-09-18 01:15:20 +0530

    added hamburger menu for navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import useTeam from '../../hooks/useTeam';
 import styles from './navbar.module.css';
@@ -10,6 +11,7 @@ const links = [
 
 export default function Navbar() {
     const { team, logoutTeam } = useTeam();
+    const [menuIsVisible, setMenuIsVisible] = useState(false);
 
     const promptThenLogout = () => {
         if (window.confirm(`Logging out as team ${team.name}`)) logoutTeam();
@@ -65,6 +67,76 @@ export default function Navbar() {
                         Signup
                     </NavLink>
                 </>
+            )}
+            <div
+                className={styles.menu}
+                onClick={() => setMenuIsVisible(!menuIsVisible)}
+            >
+                {menuIsVisible ? <>X</> : <>M</>}
+            </div>
+            {menuIsVisible && (
+                <div
+                    className={styles.fullScreenNav}
+                    onClick={() => setMenuIsVisible(false)}
+                >
+                    {links.map(link => (
+                        <NavLink
+                            className={({ isActive }) =>
+                                `${styles.link} ${
+                                    isActive ? styles.active : null
+                                }`
+                            }
+                            key={link.to}
+                            to={link.to}
+                        >
+                            {link.text}
+                        </NavLink>
+                    ))}
+                    {team ? (
+                        <>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    `${styles.link} ${
+                                        isActive ? styles.active : null
+                                    }`
+                                }
+                                to='/team'
+                            >
+                                Team {team.name}: {team.currentPoints || 0}{' '}
+                                points
+                            </NavLink>
+                            <div
+                                className={styles.link}
+                                onClick={promptThenLogout}
+                            >
+                                Logout
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    `${styles.link} ${
+                                        isActive ? styles.active : null
+                                    }`
+                                }
+                                to='/login'
+                            >
+                                Login
+                            </NavLink>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    `${styles.link} ${
+                                        isActive ? styles.active : null
+                                    }`
+                                }
+                                to='/signup'
+                            >
+                                Signup
+                            </NavLink>
+                        </>
+                    )}
+                </div>
             )}
         </nav>
     );
