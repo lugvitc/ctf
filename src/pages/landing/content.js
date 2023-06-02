9a71dc9b63f40fbadbcd790a6732f28304a7cf4f
sai80082 <saicharan.guntupalli@gmail.com>
2023-06-02 23:34:10 +0530

    updated faqs

--- src/pages/landing/content.js
+++ src/pages/landing/content.js
@@ -7,14 +7,14 @@ export const info = {
 }
 export const faqs = [
     { q: 'What is the venue of the event?', a: info.Location },
-    { q: 'What is the timing of the event?', a: '10:00 AM 24th September - 10:00 AM 25th September' },
-    { q: 'What happens in Elimination 1?', a: 'Top 10-15 teams having higher points in the Quiz and CTF will advance to the next rounds of competition. Other teams have to discontinue from the competition' },
-    { q: 'Will the participants be provided with Food?', a: 'Lunch, Refreshments and Dinner(selected teams) will be provided to the participants' },
-    { q: 'Will the participants be provided with Internet connection?', a: 'Yes, a Wi-Fi connection will be provided for the participants. Participants are expected to use the Lab systems for the first 3 competitions. They can work on their laptops for the final hackathon event' },
-    { q: 'Is this a team event?', a: 'Yes it is a team event comprising of 1-4 members' },
+    { q: 'When is the event?', a: '8th June - 9th June' },
+    { q: 'What happens in Elimination 1?', a: 'Top 10 teams having higher points in the challenges will advance to the next rounds of competition. Other teams have to discontinue from the competition.' },
+    { q: 'Will the participants be provided with Food?', a: 'Only Refreshments will be provided to the participants.' },
+    { q: 'Will the participants be provided with Internet connection?', a: 'Yes, a Wi-Fi connection will be provided for the participants.' },
+    { q: 'Is this a team event?', a: 'Yes it is a team event comprising of 1-4 members.' },
     { q: 'How to register as a team?', a: 'Every participant has to register individually for the event. Team formation will happen on the event day. You may find your team mates earlier if you wish to.' },
-    { q: 'What are the winners prizes?', a: 'Prizes worth Rs. 10,000+' },
-    { q: 'Where do we register for the event?', a: 'You can scan the QR Code in the top of the page or visit https://vitchennaievents.com/technovit/ to register for the event' }
+    { q: 'What are the winners prizes?', a: 'Prizes worth Rs. 10,000+.' },
+    { q: 'Where do we register for the event?', a: 'You can scan the QR Code in the top of the page or visit https://vitchennaievents.com/conf1/ to register for the event.' }
 ];
 
 export const days = [
@@ -25,10 +25,10 @@ export const days = [
             </>
         ),
         schedule: [
-            {description :"WORKSHOP!! Our experts will guide you through practical exercises and demonstrations and how to use them in real time."},
-            {description:"Exclusive workshop will take your knowledge to new heights!"},
-            {description:"Discover the topics such as Cybersecurity, Steganography, Web Exploitation, Basic Linux, and much more!"},
-    ]
+            { description: "WORKSHOP!! Our experts will guide you through practical exercises and demonstrations and how to use them in real time." },
+            { description: "Exclusive workshop will take your knowledge to new heights!" },
+            { description: "Discover the topics such as Cybersecurity, Steganography, Web Exploitation, Basic Linux, and much more!" },
+        ]
     },
     {
         day: (
@@ -37,8 +37,8 @@ export const days = [
             </>
         ),
         schedule: [
-            {  description: 'Immerse yourself in the ultimate 3 sessions with different sets of innovative challenges.' },
-            {  description: 'Solve puzzles, find hidden clues, and prove your skills, get the trophy to become the champion.' }
+            { description: 'Immerse yourself in the ultimate 3 sessions with different sets of innovative challenges.' },
+            { description: 'Solve puzzles, find hidden clues, and prove your skills, get the trophy to become the champion.' }
         ]
     }
 ];
