cbb66a1af87c549063f60940ec3f4ce495543e1e
Kanishk <Kanishksharma2609@gmail.com>
2023-09-30 00:20:03 +0530

    font-style changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -19,8 +19,7 @@ export default function Navbar() {
 
     return (
         <nav className={styles.all}>
-            <div className={styles.logos}> <p>Ideassion : LUGVITC's</p></div>
-            <div className={styles.name}>Password : Capture.Survive.Escape</div>
+            <div className={styles.name}>Cyber-0-Day</div>
             <div className={styles.links}>
                 {links.map(link => (
                     <NavLink
